26de9e2e6fb7942a364ba90feaf34142c06c767f
Bernardo Tomas <bernardotomas.c14@gmail.com>
2026-06-01 11:03:50 -0300

    First commit

--- HelloWorld/obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d629d07564d779d90d232611afd7b91c5b2cf1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b70c3502f9ace11ebb7976b3e7b4ffc9f6581fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
