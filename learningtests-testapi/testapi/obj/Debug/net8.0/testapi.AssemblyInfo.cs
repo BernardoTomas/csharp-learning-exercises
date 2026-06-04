294266c8a408756999eaf3b7c910d00865c66ced
Bernardo Tomas <bernardotomas.c14@gmail.com>
2026-06-04 17:47:35 -0300

    C# APIs - made an integration test to test the /GET request for the /clients route

--- learningtests-testapi/testapi/obj/Debug/net8.0/testapi.AssemblyInfo.cs
+++ learningtests-testapi/testapi/obj/Debug/net8.0/testapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24fd3f31db1807043c03c61b682e2dc18c0a7ce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0972e1ef0d5e2349314b335cfa2ef5af66f125e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("testapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
