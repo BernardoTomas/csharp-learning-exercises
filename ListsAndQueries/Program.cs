cc3be4867d4512570ee6bdfb658584efda43a1c9
Bernardo Tomas <bernardotomas.c14@gmail.com>
2026-06-27 10:04:22 -0300

    C# Lists and Queries - I created the ListsAndQueries project

---
+++ ListsAndQueries/Program.cs
@@ -0,0 +1,7 @@
+﻿List<string> cars = new List<string>(){ "Fusca" };
+
+cars.Add("Brasilia");
+
+cars.ForEach(car => Console.WriteLine(car) );
+
+List<string> brands = new List<string>() { "Samsung", "Apple", "Guaraná Dolly" };
