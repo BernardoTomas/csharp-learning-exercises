36f2b25ca027641b366c76983d0a9c4fa991eac8
Bernardo Tomas <bernardotomas.c14@gmail.com>
2026-07-15 11:02:26 -0300

    C# SQL Server and Entity Framework - Made the Students table in the db, code first

--- Aeroporto/obj/Debug/net8.0/Aeroporto.AssemblyInfo.cs
+++ Aeroporto/obj/Debug/net8.0/Aeroporto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aeroporto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a11e89c7c1238723d19e8adeaea23e67cb28539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d766ac26fa37eca0b789a96e7fcc9fa35ef614d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aeroporto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aeroporto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
