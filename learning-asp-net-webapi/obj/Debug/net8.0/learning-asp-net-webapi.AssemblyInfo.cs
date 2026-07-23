4fcd9986edbfdfe3bac273978585c1948829ff4a
Bernardo Tomas <bernardotomas.c14@gmail.com>
2026-07-23 10:17:18 -0300

    Closing branch

--- learning-asp-net-webapi/obj/Debug/net8.0/learning-asp-net-webapi.AssemblyInfo.cs
+++ learning-asp-net-webapi/obj/Debug/net8.0/learning-asp-net-webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learning-asp-net-webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b0d41777bcb7af07e059c8e44a3666718e2a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28ea3a42941cfb43c7508db55bbb75f753bc900")]
 [assembly: System.Reflection.AssemblyProductAttribute("learning-asp-net-webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learning-asp-net-webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
